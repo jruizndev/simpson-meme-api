96ec745578d48335b0cc9d2dd7dcceee306965ac
Diallo2024 <urymamadu@gmail.com>
2024-09-25 23:39:13 -0700

    Put method added

--- src/router/meme-route.js
+++ src/router/meme-route.js
@@ -4,36 +4,36 @@ const { faker } = require('@faker-js/faker')
 const Meme = require('../models/memeModels.js');
 const { where } = require('sequelize');
 
-router.get('/memes', async(req, res) => {
+router.get('/memes', async (req, res) => {
     const memes = await memes.findAll()
     res.status(200).json({
         ok: true,
         status: 200,
         body: memes
-    })
+    });
 });
 
-router.get('/memes/:meme_id', async(req, res) => {
+router.get('/memes/:meme_id', async (req, res) => {
     const id = req.params.meme_id
     const meme = await memes.findOne({
         where: {
             meme_id: id,
         }
-    })
+    });
     res.status(200).json({
         ok: true,
         status: 200,
         body: meme,
-    })
+    });
 });
 
-router.post('/memes', async(req, res) => {
+router.post('/memes', async (req, res) => {
     const dataMemes = req.body
     await Meme.sync()
     const creatMeme = await Meme.create({
         meme_name: dataMemes.meme_name,
         url: faker.commerce.url(),
-    })
+    });
     res.status(201).json({
         ok: true,
         status: 201,
@@ -41,8 +41,25 @@ router.post('/memes', async(req, res) => {
     });
 });
 
-router.put('/memes', (req, res) => {
-    res.send('soy tu router')
+router.put('/memes/:meme_id', async (req, res) => {
+    const id = req.params.meme_id;
+    const dataMemes = req.body;
+    const updateMeme = await memes.update({
+        meme_name: dataMemes.meme_name,
+        url: faker.commerce.url(),
+    },
+        {
+            where: {
+                meme_id: id,
+            }
+        });
+
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: updateMeme
+    });
+
 });
 
 router.delete('/memes', (req, res) => {
