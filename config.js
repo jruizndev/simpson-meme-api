e3a22681d43a96b2e231f66e3ee809194cd8b159
Smith Grisales <developers.smith@gmail.com>
2024-09-30 10:09:36 +0200

    config config.js to use .env and add readme with postman

--- config.js
+++ config.js
@@ -2,11 +2,12 @@
 
 const config = {
   db: {
-    name: "meme_app", // Nombre de la base de datos
-    user: "root", // Usuario de la base de datos
-    password: "tu_contraseña", // Contraseña de MySQL
-    host: "localhost", // Host donde está corriendo la base de datos
-    dialect: "mysql", // Dialecto (mysql en este caso)
+    // Utilizamos las variables de entorno para configurar la base de datos
+    name: process.env.DB_NAME, // Nombre de la base de datos desde variable de entorno
+    user: process.env.DB_USER, // Usuario de la base de datos desde variable de entorno
+    password: process.env.DB_PASSWORD, // Contraseña de MySQL desde variable de entorno
+    host: process.env.DB_HOST || "localhost", // Host desde variable de entorno, con valor por defecto
+    dialect: process.env.DB_DIALECT || "mysql", // Dialecto desde variable de entorno, con valor por defecto
   },
 };
 
