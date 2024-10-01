5a8d92b9fe6a45a19205cc957e5395a112504b1e
jruizndev <jorsqn @gmail.com>
2024-10-01 10:35:05 +0200

    retoques validaciones

--- validators/memeValidator.js
+++ validators/memeValidator.js
@@ -1,13 +1,13 @@
-import { check } from "express-validator";
+import { check } from 'express-validator'
 
 // Validaciones para la creación de un meme
 export const validateMemeCreation = [
-  check("name", "El nombre es obligatorio").not().isEmpty(), // Verifica que 'name' no esté vacío
-  check("urlImage", "La URL debe ser válida").isURL(), // Verifica que 'urlImage' sea una URL válida
-];
+    check('name', 'El nombre es obligatorio').not().isEmpty(), // Verifica que 'name' no esté vacío
+    check('image', 'La URL debe ser válida').isURL(), // Verifica que 'urlImage' sea una URL válida
+]
 
 // Validaciones para la actualización de un meme
 export const validateMemeUpdate = [
-  check("name", "El nombre es obligatorio").not().isEmpty(), // Verifica que 'name' no esté vacío
-  check("urlImage", "La URL debe ser válida").isURL(), // Verifica que 'urlImage' sea una URL válida
-];
+    check('name', 'El nombre es obligatorio').not().isEmpty(), // Verifica que 'name' no esté vacío
+    check('image', 'La URL debe ser válida').isURL(), // Verifica que 'urlImage' sea una URL válida
+]
