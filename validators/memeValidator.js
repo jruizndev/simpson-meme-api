e3f6ef8c3ecdc7aaa1598ba57b61aa4bae995d55
arthurmm77 <thurgamer77@gmail.com>
2024-10-01 12:03:38 +0200

    Add name simpsons validation

--- validators/memeValidator.js
+++ validators/memeValidator.js
@@ -1,13 +1,27 @@
-import { check } from 'express-validator'
+import { check, body } from "express-validator";
+import { simpsonsNames } from "./simpsonsNames.js";
 
-// Validaciones para la creación de un meme
+const simpsonsNamesLowerCase = simpsonsNames.map((name) => name.toLowerCase());
+// Validador para la creación de memes
 export const validateMemeCreation = [
-    check('name', 'El nombre es obligatorio').not().isEmpty(), // Verifica que 'name' no esté vacío
-    check('image', 'La URL debe ser válida').isURL(), // Verifica que 'urlImage' sea una URL válida
-]
+  check("name", "El nombre es obligatorio").not().isEmpty(),
+  body("name")
+    .custom((value) => {
+      // Convertimos el valor ingresado a minúsculas y lo comparamos
+      return simpsonsNamesLowerCase.includes(value.toLowerCase());
+    })
+    .withMessage("El nombre debe ser de un personaje de Los Simpson"),
+  check("image", "La URL debe ser válida").isURL(),
+];
 
-// Validaciones para la actualización de un meme
+// Validador para la actualización de memes
 export const validateMemeUpdate = [
-    check('name', 'El nombre es obligatorio').not().isEmpty(), // Verifica que 'name' no esté vacío
-    check('image', 'La URL debe ser válida').isURL(), // Verifica que 'urlImage' sea una URL válida
-]
+  check("name", "El nombre es obligatorio").not().isEmpty(),
+  body("name")
+    .custom((value) => {
+      // Convertimos el valor ingresado a minúsculas y lo comparamos
+      return simpsonsNamesLowerCase.includes(value.toLowerCase());
+    })
+    .withMessage("El nombre debe ser de un personaje de Los Simpson"),
+  check("image", "La URL debe ser válida").isURL(),
+];
