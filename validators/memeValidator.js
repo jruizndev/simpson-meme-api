8d310539edbc17124e5e5415d83a3475834734bc
arthurmm77 <thurgamer77@gmail.com>
2024-10-10 15:09:01 +0200

    add validation names simpsons

--- validators/memeValidator.js
+++ validators/memeValidator.js
@@ -4,12 +4,26 @@ import { simpsonsNames } from "./simpsonsNames.js";
 const simpsonsNamesLowerCase = simpsonsNames.map((name) => name.toLowerCase());
 // Validador para la creación de memes
 export const validateMemeCreation = [
-  check("name", "El nombre es obligatorio").not().isEmpty(), // Verifica que 'name' no esté vacío
-  check("image", "La URL debe ser válida").isURL(), // Verifica que 'urlImage' sea una URL válida
+  check("name", "El nombre es obligatorio").not().isEmpty(),
+  body("name")
+    .custom((value) => {
+      return simpsonsNamesLowerCase.includes(value.toLowerCase());
+    })
+    .withMessage("El nombre debe ser de un personaje de Los Simpson"),
+  check("image", "La URL debe ser válida").isURL(),
 ];
 
 // Validador para la actualización de memes
 export const validateMemeUpdate = [
-  check("name", "El nombre es obligatorio").not().isEmpty(), // Verifica que 'name' no esté vacío
-  check("image", "La URL debe ser válida").isURL(), // Verifica que 'urlImage' sea una URL válida
+  check("name", "El nombre es obligatorio").not().isEmpty(),
+  body("name")
+    .custom((value) => {
+      return simpsonsNamesLowerCase.includes(value.toLowerCase());
+    })
+    .withMessage("El nombre debe ser de un personaje de Los Simpson"),
+  check("urlImage", "La URL debe ser válida").isURL({
+    protocols: ["http", "https"],
+    require_protocol: true,
+    require_valid_protocol: true,
+  }),
 ];
