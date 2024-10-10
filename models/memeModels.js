f06dac7711df898bcea49d04537605fb74ddfbfa
arthurmm77 <thurgamer77@gmail.com>
2024-10-10 10:54:46 +0200

    Add changes validations

--- models/memeModels.js
+++ models/memeModels.js
@@ -1,27 +1,27 @@
-import connection_db from '../database/connectionDb.js'
-import { DataTypes } from 'sequelize'
+import connection_db from "../database/connectionDb.js";
+import { DataTypes } from "sequelize";
 
 const memeModel = connection_db.define(
-    'Characters',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        image: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  "Characters",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
 
-    {
-        timestamps: false,
-    }
-)
+  {
+    timestamps: false,
+  }
+);
 
-export default memeModel
+export default memeModel;
--- validators/memeValidator.js
+++ validators/memeValidator.js
@@ -7,7 +7,6 @@ export const validateMemeCreation = [
   check("name", "El nombre es obligatorio").not().isEmpty(),
   body("name")
     .custom((value) => {
-      // Convertimos el valor ingresado a minúsculas y lo comparamos
       return simpsonsNamesLowerCase.includes(value.toLowerCase());
     })
     .withMessage("El nombre debe ser de un personaje de Los Simpson"),
@@ -19,9 +18,12 @@ export const validateMemeUpdate = [
   check("name", "El nombre es obligatorio").not().isEmpty(),
   body("name")
     .custom((value) => {
-      // Convertimos el valor ingresado a minúsculas y lo comparamos
       return simpsonsNamesLowerCase.includes(value.toLowerCase());
     })
     .withMessage("El nombre debe ser de un personaje de Los Simpson"),
-  check("image", "La URL debe ser válida").isURL(),
+  check("urlImage", "La URL debe ser válida").isURL({
+    protocols: ["http", "https"],
+    require_protocol: true,
+    require_valid_protocol: true,
+  }),
 ];
